4a7921c5f13a07b3e75a9a2acc6b1eaa44734e7d
huybd <21520039@gm.uit.edu.vn>
2023-10-11 13:17:40 +0700

    code 1.4

---
+++ Bai1_4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Bai1_4
+    {
+        public static void Main(string[] args)
+        {
+            NhietKe t = new NhietKe();
+            Random rnd = new Random();
+
+            t.TempChanged += _TempChanged;
+
+            for (int i = 0; i < 20; i++)
+            {
+                t.Temprature = Math.Round((rnd.NextDouble() + rnd.Next(-80, 80)), 2);
+            }
+
+        }
+
+        private static void _TempChanged (object? sender, TempratureChangedArr n)
+        {
+            Console.WriteLine("Cap nhat nhiet do: " + n.TempratureEV);
+        }
+    }
+
+}
---
+++ Temprature.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class NhietKe
+{
+	private double _Temprature;
+
+	public double Temprature
+	{
+		get { return _Temprature; }
+		set { 
+			_Temprature = value;
+			TempratureChange(value);
+		}
+	}
+
+	private event EventHandler<TempratureChangedArr> _TempChanged;
+	public event EventHandler<TempratureChangedArr> TempChanged
+	{
+		add { _TempChanged += value; }
+		remove { _TempChanged -= value; }
+	}
+
+	void TempratureChange(double value)
+	{
+		if (_TempChanged != null)
+		{
+			_TempChanged(this, new TempratureChangedArr(value));
+		}
+	}
+}
+
+public class TempratureChangedArr : EventArgs
+{
+	public double TempratureEV { get; set; }
+	public TempratureChangedArr(double value)
+	{
+		this.TempratureEV = value;
+	}
+}
+

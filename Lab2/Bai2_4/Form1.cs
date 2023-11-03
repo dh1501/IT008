5a12858b420168794944ef1dfd4680cfb59e056e
Le Huu Dat <146201680+datlh9103@users.noreply.github.com>
2023-11-03 16:43:14 +0700

    Add files via upload

---
+++ Lab2/Bai2_4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2_4
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font drawFont = new Font("Arial", 12);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            int x = random.Next(0, this.Width);
+            int y = random.Next(0, this.Height);
+            g.DrawString("Paint Event", drawFont, drawBrush, x, y);
+        }
+    }
+}
